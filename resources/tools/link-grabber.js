99ab6a4d3c86998a8de30c1a2f733bd9492be10d
Seth Clydesdale <sethc1995@gmail.com>
2018-02-27 21:28:22 -0500

    add page title to exercises
    Should make it more clear what exercise the student is viewing. Also
    updated the link grabber to include a minify option, since we may want
    to minify the output in the future.

--- resources/tools/link-grabber.js
+++ resources/tools/link-grabber.js
@@ -2,14 +2,15 @@
 // This script should be executed in your console while viewing the index
 // Once executed it'll copy the array; you can then use this array to update the `exercises` array in javascript.js
 (function() {
-  var a = document.querySelectorAll('a[href*="lessons"]'), // grab all exercise links; they're in the "lessons" folder
+  var minify = false, // minifies the output if set to true
+      a = document.querySelectorAll('a[href*="lessons"]'), // grab all exercise links; they're in the "lessons" folder
       i = 0,
       j = a.length,
-      str = '[ // exercise list\n'; // open and start the array
+      str = '[' + (minify ? '' : ' // exercise list\n'); // open and start the array
   
   // loop over the links and parse them into a neat {key:value} pair
   for (; i < j; i++) {
-    str += '  \'' + a[i].href.replace(/.*?\/lessons\/(.*?\/.*?)\/.*/g, '$1') + '|' + a[i].parentNode.innerText.replace(/(.*?)\s\((.*?)\)/, '$1|$2') + '\'' + ( i == j - 1 ? '' : ',' ) + '\n'
+    str += (minify ? '' : '  ') + '\'' + a[i].href.replace(/.*?\/lessons\/(.*?\/.*?)\/.*/g, '$1') + '|' + a[i].parentNode.innerText.replace(/(.*?)\s\((.*?)\)/, '$1|$2') + '\'' + ( i == j - 1 ? '' : ',' ) + (minify ? '' : '\n')
   }
 
   str += ']'; // close the array
