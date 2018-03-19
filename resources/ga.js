cd6c14f2d81a676f44fe74d6716c4c1e79d8411c
Seth Clydesdale <sethc1995@gmail.com>
2018-03-19 09:44:31 -0400

    revert ga.js changes
    gtag adds unnecessary bulk. Also removed the fa comment from the
    stylesheet to reduce size, credit is still given within the readme.

--- resources/ga.js
+++ resources/ga.js
@@ -1,19 +1,10 @@
 // Google Analytics
 // disabled if used locally or on another host
 if (window.location.hostname == 'sethclydesdale.github.io') {
-  (function () {
-    var tid = 'UA-114289226-1',
-        ga = document.createElement('SCRIPT');
-
-    ga.src = 'https://www.googletagmanager.com/gtag/js?id=' + tid;
-    ga.type = 'text/javascript';
-    ga.async = true;
-    document.getElementsByTagName('HEAD')[0].appendChild(ga);
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', tid);
-  }());
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-114289226-1', 'auto');
+  ga('send', 'pageview');
 }
