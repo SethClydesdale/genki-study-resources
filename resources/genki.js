4d006e9ce068dff067ce0e443adafaea1161b018
Seth Clydesdale <sethc1995@gmail.com>
2018-05-09 14:13:44 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 6
    - Practice: ～てもいいです／～てはいけません

--- resources/genki.js
+++ resources/genki.js
@@ -340,7 +340,8 @@
       'lesson-6/grammar-2|Practice: Te-form Conjugation|p.156; I-A',
       'lesson-6/grammar-3|Practice: ～てください|p.156-157; I-C & D',
       'lesson-6/grammar-4|Practice: ～てもいいですか|p.158; II-A & B',
-      'lesson-6/grammar-5|Practice: ～てはいけません|p.159; III-A'
+      'lesson-6/grammar-5|Practice: ～てはいけません|p.159; III-A',
+      'lesson-6/grammar-6|Practice: ～てもいいです／～てはいけません|p.159; III-C'
     ],
 
 
