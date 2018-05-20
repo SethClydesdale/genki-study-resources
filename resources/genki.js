fbfca109666d289ef62f19f0a69c2acfb625300e
Seth Clydesdale <sethc1995@gmail.com>
2018-05-20 14:18:51 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 6
    - Reading Practice: Bulletin Board

--- resources/genki.js
+++ resources/genki.js
@@ -364,7 +364,8 @@
       'lesson-6/literacy-6|Kanji Practice: 大 and 学|p.313',
       'lesson-6/literacy-7|Kanji Practice: 外 and 国|p.313',
       'lesson-6/literacy-8|Kanji Practice: Combine the Kanji|p.314; I-A',
-      'lesson-6/literacy-9|Reading Practice: Find the Location|p.314; I-B'
+      'lesson-6/literacy-9|Reading Practice: Find the Location|p.314; I-B',
+      'lesson-6/literacy-10|Reading Practice: Bulletin Board|p.314-315; II'
     ],
 
 
