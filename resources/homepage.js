973a59c8c0f8cf2e87fd88ab7c17ca57e60c3618
Seth Clydesdale <sethc1995@gmail.com>
2018-05-25 13:07:40 -0400

    update quick search functionality
    - added resume search and the ability to initiate searches via a query in the url. (URL?search=query)
    - added a small counter and button for copying direct search links.
    - fixed searches including "coming soon..." in the results.

--- resources/homepage.js
+++ resources/homepage.js
@@ -3,30 +3,41 @@
   'use strict';
   
   // # QUICK SEARCH #
-  var results = document.getElementById('quick-search-results'),
+  var search = document.getElementById('quick-search'),
+      results = document.getElementById('quick-search-results'),
+      hitsCounter = document.getElementById('quick-search-hits'),
       li = document.querySelectorAll('.lesson-exercises li'),
-      i = 0,
-      j = li.length,
-      frag, clone;
-
-  // search for exercises that contain a specific keyword
-  document.getElementById('quick-search').oninput = function () {
-    frag = document.createDocumentFragment();
+      exLen = li.length;
+  
+  // search function
+  function quickSearch (value) {
+    var frag = document.createDocumentFragment(),
+        hits = 0,
+        i = 0,
+        clone;
+    
+    // clear prior searches
     results.innerHTML = '';
 
-    if (this.value) {
-      for (i = 0; i < j; i++) {
-        if (li[i].innerText.toLowerCase().indexOf(this.value.toLowerCase()) != -1) {
-          clone = li[i].cloneNode(true);
+    // loop over the exercises if a value is present
+    if (value) {
+      for (; i < exLen; i++) {
+        if (li[i].innerText.toLowerCase().indexOf(value.toLowerCase()) != -1 && li[i].getElementsByTagName('A')[0]) {
+          clone = li[i].cloneNode(true); // clone the match for displaying in the results node
           
           // add lesson number to exercise
-          clone.dataset.lesson = clone.querySelector('A').href.replace(/.*?(lesson-\d+).*/, function (Match, $1) {
+          clone.dataset.lesson = clone.getElementsByTagName('A')[0].href.replace(/.*?(lesson-\d+).*/, function (Match, $1) {
             return $1.charAt(0).toUpperCase() + $1.split('-').pop();
           });
           
-          clone.title = li[i].innerText; // add tooltip in case the text gets cut off
+          // add tooltip in case the text gets cut off
+          clone.title = li[i].innerText;
           
+          // add the clone to the fragment
           frag.appendChild(clone);
+          
+          // increment hits
+          hits++;
         }
       }
     }
@@ -36,7 +47,51 @@
       results.appendChild(frag);
 
     } else {
-      results.innerHTML = this.value ? '<li>No exercises found for "' + this.value + '".</li>' : '';
+      results.innerHTML = value ? '<li>No exercises found for "' + value + '".</li>' : '';
     }
+    
+    // update the hits counter and add a button to copy the search link
+    hitsCounter.innerHTML = hits ? '(' + hits + ') '+
+      '<a '+
+        'class="fa" '+
+        'style="color:#F60;" '+
+        'href="#copy-search-link" '+
+        'onclick="'+
+          'window.prompt('+
+            '\'You can copy the direct search link from the box below.\', '+
+            '\'' + (window.location.protocol + '//' + window.location.host + window.location.pathname) + '?search=' + encodeURIComponent(value) + '\''+
+          '); '+
+          'return false;'+
+        '"'+
+      '>&#xf0ea;</a>' : '';
   };
+  
+  
+  // set the value of the search field via the url (e.g. ?search=kanji)
+  if (window.location.search) {
+    var query = window.location.search.slice(1).split('&'),
+        i = 0,
+        j = query.length,
+        keyVal;
+    
+    for (; i < j; i++) {
+      keyVal = query[i].split('=');
+      
+      if (/search/i.test(keyVal[0])) {
+        search.value = decodeURIComponent(keyVal[1]);
+        break;
+      }
+    }
+  }
+  
+  // search for exercises when the user inputs text
+  search.oninput = function () {
+    quickSearch(this.value);
+  };
+  
+  // resume previous searches (in the event the user goes back in history) or those initiated by ?search=query
+  if (search.value) {
+    quickSearch(search.value);
+  }
+  
 }(window, document));
