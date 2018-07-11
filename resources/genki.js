0ff73d08c80ed5849baf8b9397082e2cc2397074
Seth Clydesdale <sethc1995@gmail.com>
2018-07-11 13:29:08 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 10
    - Practice: Comparison between Two Items

--- resources/genki.js
+++ resources/genki.js
@@ -499,7 +499,8 @@
       'lesson-10/vocab-2|Vocabulary: Nouns 2|p.230',
       'lesson-10/vocab-3|Vocabulary: い-adjectives and な-adjectives|p.230-231',
       'lesson-10/vocab-4|Vocabulary: U-verbs, Ru-verbs, and Irregular Verbs|p.231',
-      'lesson-10/vocab-5|Vocabulary: Adverbs and Other Expressions|p.231'
+      'lesson-10/vocab-5|Vocabulary: Adverbs and Other Expressions|p.231',
+      'lesson-10/grammar-1|Practice: Comparison between Two Items|p.237; I-A'
     ],
 
 
