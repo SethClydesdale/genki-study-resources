179f9a7d668fbadb32653782fbf3fe411e503406
Seth Clydesdale <sethc1995@gmail.com>
2018-07-27 12:57:43 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 11
    - Practice: ～たい

--- resources/genki.js
+++ resources/genki.js
@@ -543,7 +543,8 @@
       'lesson-11/vocab-2|Vocabulary: Nouns 2|p.252',
       'lesson-11/vocab-3|Vocabulary: U-verbs, Ru-verbs, and Irregular Verbs|p.252-253',
       'lesson-11/vocab-4|Vocabulary: Adverbs and Other Expressions|p.253',
-      'lesson-11/vocab-5|Vocabulary: Occupations|p.253'
+      'lesson-11/vocab-5|Vocabulary: Occupations|p.253',
+      'lesson-11/grammar-1|Practice: ～たい|p.259; I-A'
     ],
 
 
