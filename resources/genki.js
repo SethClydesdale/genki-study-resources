3aa2ad52bf81f4d4027c771db0e84427ddcaf25d
Seth Clydesdale <sethc1995@gmail.com>
2018-07-30 12:00:12 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 11
    - Practice: ～ことがある
    - Practice: Noun A や Noun B

--- resources/genki.js
+++ resources/genki.js
@@ -546,7 +546,9 @@
       'lesson-11/vocab-5|Vocabulary: Occupations|p.253',
       'lesson-11/grammar-1|Practice: ～たい|p.259; I-A',
       'lesson-11/grammar-2|Practice: ～たい (Past Tense)|p.259; I-C',
-      'lesson-11/grammar-3|Practice: ～たり～たりする|p.261; II-A'
+      'lesson-11/grammar-3|Practice: ～たり～たりする|p.261; II-A',
+      'lesson-11/grammar-4|Practice: ～ことがある|p.262; III-A',
+      'lesson-11/grammar-5|Practice: Noun A や Noun B|p.263; IV-A'
     ],
 
 
