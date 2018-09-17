29a31f8fd7ad8f852c64deeec6bce964e8b9c05e
Seth Clydesdale <sethc1995@gmail.com>
2018-09-17 11:56:07 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 13
    - Reading Practice: 満員電車

--- resources/genki.js
+++ resources/genki.js
@@ -610,7 +610,8 @@
       'lesson-13/literacy-5|Kanji Practice: 着, 同, 海, and 昼|p.277',
       'lesson-13/literacy-6|Kanji Practice: Write the Readings|p.278; I-A & B',
       'lesson-13/literacy-7|Reading Practice: Identify the Food|p.279; II-A',
-      'lesson-13/literacy-8|Reading Practice: 日本のおもしろい経験|p.279-280; II-B & C'
+      'lesson-13/literacy-8|Reading Practice: 日本のおもしろい経験|p.279-280; II-B & C',
+      'lesson-13/literacy-9|Reading Practice: 満員電車|p.281-282; III-B & C'
     ],
 
 
