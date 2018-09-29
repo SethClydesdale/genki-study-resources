514525be2ad1568687c64213ed5f9ac764c042a3
Seth Clydesdale <sethc1995@gmail.com>
2018-09-29 11:39:08 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 14
    - Workbook: あげる／くれる／もらう

--- resources/genki.js
+++ resources/genki.js
@@ -637,7 +637,8 @@
       'lesson-14/culture-1|Culture Note: Annual Events in Japan|p.72',
       'lesson-14/vocab-6|Useful Expressions: Counters|p.73',
       'lesson-14/workbook-1|Workbook: ほしい|p.21; I, II, & III',
-      'lesson-14/workbook-2|Workbook: ～かもしれません|p.22; I & II'
+      'lesson-14/workbook-2|Workbook: ～かもしれません|p.22; I & II',
+      'lesson-14/workbook-3|Workbook: あげる／くれる／もらう|p.23; I & II'
     ],
 
 
