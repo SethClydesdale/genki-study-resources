db9dd468953ba7fd2901c93f2ac33d9e915534fd
Seth Clydesdale <sethc1995@gmail.com>
2018-10-20 13:51:48 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 15
    - Reading Practice: 私が好きな所

--- resources/genki.js
+++ resources/genki.js
@@ -682,7 +682,8 @@
       'lesson-15/literacy-3|Kanji Practice: 広, 転, and 借|p.288-289',
       'lesson-15/literacy-4|Kanji Practice: 走, 建, and 地|p.289',
       'lesson-15/literacy-5|Kanji Practice: 場, 足, and 通|p.289',
-      'lesson-15/literacy-6|Kanji Practice: Write the Readings|p.290; I-A & B'
+      'lesson-15/literacy-6|Kanji Practice: Write the Readings|p.290; I-A & B',
+      'lesson-15/literacy-7|Reading Practice: 私が好きな所|p.291-294; II-B, C, & B'
     ],
 
 
