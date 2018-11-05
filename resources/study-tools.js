01f311f3bb4c235647db9a33a4bac233a46e1d2c
Seth Clydesdale <sethc1995@gmail.com>
2018-11-05 12:19:36 -0500

    add custom spelling tool, update links, fix bug
    - fixed a small bug in the quick search.
    - updated links for custom exercises; the folders are now prefixed with "custom-".
    - added style for number inputs.
    - added "Custom Spelling Practice" to study tools.

--- resources/study-tools.js
+++ resources/study-tools.js
@@ -1,5 +1,4 @@
 // tools used for creating custom exercises and other tools used for studying
-// TODO: Custom Spelling Practice
 // TODO: Custom Quiz
 // TODO: Custom Fill in the Blanks
 Genki.tools = {
@@ -40,25 +39,33 @@ Genki.tools = {
     }
 
     var data = JSON.parse(code.value),
+        buttons = 
+        '<button class="button" title="add another word" onclick="Genki.tools.addRow(this);"><i class="fa">&#xf067;</i></button>'+
+        '<button class="button" title="remove word" onclick="Genki.tools.removeRow(this);"><i class="fa">&#xf068;</i></button>',
         str = '',
         i;
 
-    // fromatting for vocab rows
+    // formatting for vocab rows
     if (this.type == 'vocab') {
-      var word, furigana, definition;
-      
       for (i in data) {
-        word = /\|/.test(i) ? i.split('|')[0] : i;
-        furigana = /\|/.test(i) ? i.split('|')[1] : '';
-        definition = data[i];
-
         str += 
-          '<li class="item-row">'+
-          '<input type="text" placeholder="word/kanji" oninput="Genki.tools.updateJSON();" value="' + word + '">&nbsp;'+
-          '<input type="text" placeholder="furigana (optional)" oninput="Genki.tools.updateJSON();" value="' + furigana + '">&nbsp;'+
-          '<input type="text" placeholder="definition/kana" oninput="Genki.tools.updateJSON();" value="' + definition + '">&nbsp;'+
-          '<button class="button" title="add another word" onclick="Genki.tools.addRow(this);"><i class="fa">&#xf067;</i></button>'+
-          '<button class="button" title="remove word" onclick="Genki.tools.removeRow(this);"><i class="fa">&#xf068;</i></button>'+
+        '<li class="item-row">'+
+          '<input type="text" placeholder="word/kanji" oninput="Genki.tools.updateJSON();" value="' + (/\|/.test(i) ? i.split('|')[0] : i) + '">&nbsp;'+
+          '<input type="text" placeholder="furigana (optional)" oninput="Genki.tools.updateJSON();" value="' + (/\|/.test(i) ? i.split('|')[1] : '') + '">&nbsp;'+
+          '<input type="text" placeholder="definition/kana" oninput="Genki.tools.updateJSON();" value="' + data[i] + '">&nbsp;'+
+          buttons+
+        '</li>';
+      }
+    }
+    
+    // formatting for spelling rows
+    else if (this.type == 'spelling') {
+      for (i in data) {
+        str += 
+        '<li class="item-row">'+
+          '<input type="text" placeholder="word/kanji" oninput="Genki.tools.updateJSON();" value="' + i + '">&nbsp;'+
+          '<input type="text" placeholder="furigana (optional)" oninput="Genki.tools.updateJSON();" value="' + (data[i] ? data[i] : '') + '">&nbsp;'+
+          buttons+
         '</li>';
       }
     }
@@ -86,13 +93,25 @@ Genki.tools = {
         code[input[0].value + (input[1].value ? '|' + input[1].value : '')] =  input[2].value;
       }
     }
+    
+    // code formatting for spelling
+    else if (this.type == 'spelling') {
+      for (; i < j; i++) {
+        input = row[i].getElementsByTagName('INPUT');
+
+        // 0 = word/kanji
+        // 1 = furigana
+        code[input[0].value] = input[1].value;
+      }
+    }
 
     json = document.getElementById('prettyCode').checked ? JSON.stringify(code, '', '  ') : JSON.stringify(code);
     document.getElementById('study-tool-json').value = json;
     
     // save JSON to localStorage
     window.localStorage[{
-      vocab : 'customVocab'
+      vocab : 'customVocab',
+      spelling : 'customSpelling'
     }[this.type]] = json;
   },
   
@@ -100,12 +119,14 @@ Genki.tools = {
   // restores data saved to localStorage
   restore : function () {
     var type = {
-      vocab : 'customVocab'
+      vocab : 'customVocab',
+      spelling : 'customSpelling'
     }[this.type];
     
     if (window.localStorage[type]) {
       document.getElementById('study-tool-json').value = window.localStorage[type];
       this.updateUI();
+      this.updateJSON();
 
     } else {
       this.updateJSON();
@@ -116,21 +137,31 @@ Genki.tools = {
   // begin studying a custom exercise
   study : function () {
     if (document.getElementById('noStudyWarning').checked || confirm('Are you sure you\'re ready to study? Your custom exercise will be temporarily saved to the browser cache, however, if you want to use it again later, click "cancel", then copy the code and save it to a text document. (click "do not warn me" to disable this message)')) {
-
+      var quizlet = document.getElementById('study-tool-json').value;
+      
       document.getElementById('study-tool-editor').style.display = 'none';
       document.getElementById('exercise').style.display = '';
       
-      // generate a quiz based on the tool type
-      Genki.generateQuiz({
-        // generate a vocab exercise
-        vocab : {
+      // generate a vocab exercise
+      if (this.type == 'vocab') {
+        Genki.generateQuiz({
           type : 'drag',
           info : 'Match the definition/kana to the word/kanji.',
 
-          quizlet : JSON.parse(document.getElementById('study-tool-json').value)
-        }
-      }[this.type]);
+          quizlet : JSON.parse(quizlet)
+        });
+      }
+      
+      // generate a spelling exercise
+      else if (this.type == 'spelling') {
+        Genki.generateQuiz({
+          type : 'writing',
+          info : 'Practice spelling the following words.',
 
+          columns : +document.getElementById('spellingColumns').value,
+          quizlet : JSON.parse(quizlet)
+        });
+      }
     }
   },
   
