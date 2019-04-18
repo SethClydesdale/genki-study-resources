c49a2640d21417c05e727267d88396ebfd58f178
Seth Clydesdale <sethc1995@gmail.com>
2019-04-18 12:20:00 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 18
    - Practice: ～てしまう 2

--- resources/genki.js
+++ resources/genki.js
@@ -782,6 +782,7 @@
       'lesson-18/grammar-1|Practice: Transitivity Pairs|p.150; I-A',
       'lesson-18/grammar-2|Practice: Transitivity Pairs + ている|p.151-152; I-C',
       'lesson-18/grammar-3|Practice: ～てしまう 1|p.153; II-A',
+      'lesson-18/grammar-4|Practice: ～てしまう 2|p.153; II-B',
       
       // Study Tools
       'study-tools/custom-vocab|Custom Vocabulary Practice',
