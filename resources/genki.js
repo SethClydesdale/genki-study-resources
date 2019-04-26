5428238110768fba7d15e14e1d7cb7073d5f0536
Seth Clydesdale <sethc1995@gmail.com>
2019-04-26 11:11:07 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 18
    - Practice: ～ばよかったです

--- resources/genki.js
+++ resources/genki.js
@@ -787,6 +787,7 @@
       'lesson-18/grammar-6|Practice: ～と|p.155; III-A',
       'lesson-18/grammar-7|Practice: ～ながら|p.157; IV-A',
       'lesson-18/grammar-8|Practice: ～ばよかったです Conjugation|p.158; V-A',
+      'lesson-18/grammar-9|Practice: ～ばよかったです|p.158-159; V-B',
       
       // Study Tools
       'study-tools/custom-vocab|Custom Vocabulary Practice',
