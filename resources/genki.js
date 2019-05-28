fd0653a3cf73a1c284058c9601213385013d1d94
Seth Clydesdale <sethc1995@gmail.com>
2019-05-28 12:02:32 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 18
    - Workbook: ～ばよかったです

--- resources/genki.js
+++ resources/genki.js
@@ -796,6 +796,7 @@
       'lesson-18/workbook-3|Workbook: ～てしまう|p.53; I & II',
       'lesson-18/workbook-4|Workbook: ～と|p.54; I, II, & III',
       'lesson-18/workbook-5|Workbook: ～ながら|p.55; I, II, & III',
+      'lesson-18/workbook-6|Workbook: ～ばよかったです|p.56; I, II, & III',
       
       // Study Tools
       'study-tools/custom-vocab|Custom Vocabulary Practice',
