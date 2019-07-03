846f18355de3e215be21fa9533bc106c8cc040e5
Seth Clydesdale <sethc1995@gmail.com>
2019-07-03 12:54:29 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 20 (last commit was for this lesson; I made a typo)
    - Practice: Extra-modest Expressions 1

--- resources/genki.js
+++ resources/genki.js
@@ -857,6 +857,7 @@
       'lesson-20/vocab-3|Vocabulary: U-verbs|p.186-187',
       'lesson-20/vocab-4|Vocabulary: Ru-verbs and Irregular Verbs|p.187',
       'lesson-20/vocab-5|Vocabulary: Adverbs and Other Expressions|p.187',
+      'lesson-20/grammar-1|Practice: Extra-modest Expressions 1|p.195; I-A',
       
       // Study Tools
       'study-tools/custom-vocab|Custom Vocabulary Practice',
