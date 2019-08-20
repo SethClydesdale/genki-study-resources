3947155bf5d4d4c4d4616116d3fd70b3c41f1d02
Seth Clydesdale <sethc1995@gmail.com>
2019-08-20 11:27:44 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 22
    - Workbook: ～のに

--- resources/javascript/genki.js
+++ resources/javascript/genki.js
@@ -949,6 +949,7 @@
       'lesson-22/workbook-3|Workbook: Causative + あげる／くれる|p.88; I & II',
       'lesson-22/workbook-4|Workbook: Verb Stem + なさい|p.89; I & II',
       'lesson-22/workbook-5|Workbook: ～ば|p.90; I, II, & III',
+      'lesson-22/workbook-6|Workbook: ～のに|p.91; I & II',
       
       // Study Tools
       'study-tools/custom-vocab|Custom Vocabulary Practice',
