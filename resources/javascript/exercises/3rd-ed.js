5013e31c0b9d63eaec9988367333efde8a0bcea0
Seth Clydesdale <sethc1995@gmail.com>
2020-10-03 10:57:27 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 8 (3rd ed.)
    - Practice: Quotations (～と思います) 1

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -350,6 +350,7 @@ window.GenkiExercises = [
   'lesson-8/grammar-2|Practice: Short Form Conjugation (Adjectives/Nouns)|p.198; I-B',
   'lesson-8/grammar-3|Practice: Informal Speech (Verbs)|p.198-199; II-A',
   'lesson-8/grammar-4|Practice: Informal Speech (Adjectives/Nouns)|p.199; II-B',
+  'lesson-8/grammar-5|Practice: Quotations (～と思います) 1|p.199; III-A',
   
   // Appendix
   //TODO:'appendix/grammar-index|Grammar Index|p.358-359',
