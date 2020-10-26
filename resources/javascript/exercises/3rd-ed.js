c19d6fd573130e75acd5817565e2a08478b8266c
Seth Clydesdale <sethc1995@gmail.com>
2020-10-26 10:57:22 -0400

    add new exercises, fix telephone number answer bug
    - fixed telephone number answers with two or more dashes being marked as incorrect answers, even when answered correctly. (3rd ed.; #107)
    - added the following exercises.
    
    # Lesson 9 (3rd ed.)
    - Practice: Quotations (～と思います)

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -395,6 +395,7 @@ window.GenkiExercises = [
   'lesson-9/grammar-2|Practice: Short Form Conjugation (Past Tense Adjectives/Nouns)|p.219-220; I-B',
   'lesson-9/grammar-3|Practice: Informal Speech (Past Tense Verbs)|p.220; II-A',
   'lesson-9/grammar-4|Practice: Informal Speech (Past Tense Adjectives/Nouns)|p.220-221; II-B',
+  'lesson-9/grammar-5|Practice: Quotations (～と思います)|p.221; III-A',
   
   // Appendix
   //TODO:'appendix/grammar-index|Grammar Index|p.358-359',
