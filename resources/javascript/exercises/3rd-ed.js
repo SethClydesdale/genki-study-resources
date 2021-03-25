dcf1f2c8ba5a975ace083bc8c4641bd1d7bf7271
Seth Clydesdale <sethc1995@gmail.com>
2021-03-25 10:18:17 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 14 (3rd ed.)
    - Practice: くれる／もらう
    - Practice: あげる／くれる／もらう
    - Practice: ～たらどうですか

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -595,6 +595,9 @@ window.GenkiExercises = [
   'lesson-14/grammar-2|Practice: ほしい (Past)|p.57; I-B',
   'lesson-14/grammar-3|Practice: ～かもしれません|p.59; II-A',
   'lesson-14/grammar-4|Practice: あげる|p.61; III-A',
+  'lesson-14/grammar-5|Practice: くれる／もらう|p.62; III-C',
+  'lesson-14/grammar-6|Practice: あげる／くれる／もらう|p.62; III-D',
+  'lesson-14/grammar-7|Practice: ～たらどうですか|p.64; IV-A',
   
   // Appendix
   'appendix/dictionary|Vocabulary Index (Dictionary)|p.352-383',
