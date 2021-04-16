eda2d432f32a43ed970fa7b4be2a06fe98c8ad5b
Seth Clydesdale <sethc1995@gmail.com>
2021-04-16 10:20:31 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 16 (3rd ed.)
    - Practice: ～ていただけませんか
    - Practice: ～といいですね

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -670,6 +670,8 @@ window.GenkiExercises = [
   'lesson-16/grammar-1|Practice: ～てあげる|p.104; I-A',
   'lesson-16/grammar-2|Practice: ～てくれる／～てもらう|p.105; I-C',
   'lesson-16/grammar-3|Practice: ～てあげる／～てくれる／～てもらう|p.105-106; I-E',
+  'lesson-16/grammar-4|Practice: ～ていただけませんか|p.107-108; II-A',
+  'lesson-16/grammar-5|Practice: ～といいですね|p.108-109; III-A',
   
   // Appendix
   'appendix/dictionary|Vocabulary Index (Dictionary)|p.352-383',
