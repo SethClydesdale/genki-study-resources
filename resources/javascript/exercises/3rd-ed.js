aee6c5b90de0b5ee8a481c54e1d3aa121a9925b0
Seth Clydesdale <sethc1995@gmail.com>
2021-04-22 10:11:15 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 16 (3rd ed.)
    - Workbook: ～時 2
    - Workbook: ～てすみませんでした

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -681,6 +681,8 @@ window.GenkiExercises = [
   'lesson-16/workbook-3|Workbook: ～ていただけませんか|p.37; I & II',
   'lesson-16/workbook-4|Workbook: ～といいですね／～といいんですが|p.38; I & II',
   'lesson-16/workbook-5|Workbook: ～時 1|p.39; I & II',
+  'lesson-16/workbook-6|Workbook: ～時 2|p.40; I & II',
+  'lesson-16/workbook-7|Workbook: ～てすみませんでした|p.41; I & II',
   
   // Appendix
   'appendix/dictionary|Vocabulary Index (Dictionary)|p.352-383',
