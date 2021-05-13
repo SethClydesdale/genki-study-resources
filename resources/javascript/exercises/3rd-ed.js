926c96969c4a8b5920567e589598c7c262907b49
Seth Clydesdale <sethc1995@gmail.com>
2021-05-13 10:18:09 -0400

    add new exercises, anki deck/wordlist
    - added anki deck/wordlist for Lesson 18 vocab. (3rd ed.)
    - added the following exercises.
    
    # Lesson 18 (3rd ed.)
    - Vocabulary: Nouns 1
    - Vocabulary: Nouns 2 and Adjectives
    - Vocabulary: U-verbs
    - Vocabulary: Ru-verbs and Irregular Verbs
    - Vocabulary: Adverbs and Other Expressions

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -735,6 +735,13 @@ window.GenkiExercises = [
   'lesson-17/literacy-wb-1|Workbook: Kanji Writing Practice|p.119',
   'lesson-17/literacy-wb-2|Workbook: Using Kanji|p.120',
   
+  // Lesson 18
+  'lesson-18/vocab-1|Vocabulary: Nouns 1|p.138',
+  'lesson-18/vocab-2|Vocabulary: Nouns 2 and Adjectives|p.138',
+  'lesson-18/vocab-3|Vocabulary: U-verbs|p.138-139',
+  'lesson-18/vocab-4|Vocabulary: Ru-verbs and Irregular Verbs|p.139',
+  'lesson-18/vocab-5|Vocabulary: Adverbs and Other Expressions|p.139',
+  
   // Appendix
   'appendix/dictionary|Vocabulary Index (Dictionary)|p.352-383',
   'appendix/map-of-japan|Map of Japan|p.384-385',
