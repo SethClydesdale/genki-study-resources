b43220e11c833440fbaf6aba02fd7bde25d20fe5
Seth Clydesdale <sethc1995@gmail.com>
2021-06-06 10:47:41 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 19 (3rd ed.)
    - Practice: ～はずです 2

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -790,6 +790,7 @@ window.GenkiExercises = [
   'lesson-19/grammar-5|Practice: ～てくれてありがとう|p.173; III-A',
   'lesson-19/grammar-6|Practice: ～てよかったです|p.174; IV-A',
   'lesson-19/grammar-7|Practice: ～はずです 1|p.175; V-A',
+  'lesson-19/grammar-8|Practice: ～はずです 2|p.176-177; V-C',
   
   // Appendix
   'appendix/dictionary|Vocabulary Index (Dictionary)|p.352-383',
