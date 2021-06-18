235d3b6fc2489532ba2ec3a82f5f978b22cc450a
Seth Clydesdale <sethc1995@gmail.com>
2021-06-18 10:21:19 -0400

    add new exercises
    - added the following exercises.
    
    # Lesson 20 (3rd ed.)
    - Practice: Questions within Larger Sentences

--- resources/javascript/exercises/3rd-ed.js
+++ resources/javascript/exercises/3rd-ed.js
@@ -829,6 +829,7 @@ window.GenkiExercises = [
   'lesson-20/grammar-6|Practice: Humble Expressions 2|p.193-194; II-B',
   'lesson-20/grammar-7|Practice: Humble Expressions 3|p.195; II-D',
   'lesson-20/grammar-8|Practice: ～ないで|p.196; III-A',
+  'lesson-20/grammar-9|Practice: Questions within Larger Sentences|p.197; IV-A',
   
   // Appendix
   'appendix/dictionary|Vocabulary Index (Dictionary)|p.352-383',
