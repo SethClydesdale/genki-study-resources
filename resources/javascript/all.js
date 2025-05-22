f280e18ac4f8982f9c1ff9ae379e82c4aabd8169
Seth Clydesdale <sethc1995@gmail.com>
2025-05-22 14:32:04 -0400

    add stream announcement
    - added announcement that shows when the developer is streaming. Current streams will consist of Quartet Study Resources development.

--- resources/javascript/all.js
+++ resources/javascript/all.js
@@ -164,6 +164,53 @@
             GenkiAnn.rotate();
           }
         }
+        
+        // Live Stream announcement (only shows when stream is active)
+        if (window.location.protocol != 'file:') {
+          window.onload = function () {
+            var script = document.createElement('SCRIPT'),
+                stream = document.createElement('DIV');
+
+            stream.id = 'stream';
+            stream.style.display = 'none';
+
+            script.src = 'https://player.twitch.tv/js/embed/v1.js';
+            script.async = true;
+            script.onload = function () {
+              var options = {
+                width: 300,
+                height: 300,
+                channel: "sethc95",
+                parent: ["sethclydesdale.github.io"],
+                muted: true,
+                autoplay: false
+              };
+
+              var player = new Twitch.Player("stream", options);
+
+              // offline; do nothing except kill the iframe
+              player.addEventListener(Twitch.Player.OFFLINE, function () {
+                stream.querySelector('iframe').src = 'about:blank';
+              });
+
+              // online; show live stream announcement and kill the iframe
+              player.addEventListener(Twitch.Player.ONLINE, function () {
+                stream.querySelector('iframe').src = 'about:blank';
+                document.querySelector('.announcement:not(.announce-hidden)').className += ' announce-hidden';
+                GenkiAnn.list.insertAdjacentHTML('afterBegin',
+                  '<div class="announcement">'+
+                    '<span class="date"><span class="t-red" style="margin-right:1px;">●</span>LIVE</span>'+
+                    'Come hang out and watch the development progress of Quartet Study Resources on <a href="https://www.twitch.tv/sethc95" target="_blank">Twitch</a>.'+
+                  '</div>'
+                );
+                GenkiAnn.msg = document.querySelectorAll('.announcement');
+              });
+            };
+
+            document.body.appendChild(script);
+            document.body.appendChild(stream);
+          };
+        }
       }
     };
     
